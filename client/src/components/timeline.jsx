91b76da2afeaa9b1726957e9e615a884152acb45
august28 <llogbi124@gmail.com>
2025-01-07 14:19:54 +0800

    fixed the margin on the timeline

--- client/src/components/timeline.jsx
+++ client/src/components/timeline.jsx
@@ -37,16 +37,15 @@ const Timeline = () => {
     // Infinite crolling for timeline
     <div
       id="posts-container"
-      className="flex-auto items-center px-auto px-6 pl-11  overflow-y-auto"
+      className="flex-auto flex flex-col items-center px-4 overflow-y-auto"
     >
-      {/* Header of the timeline */}
-      <div className="py-4 w-full bg-white-500 mb-2 flex items-center justify-between pl-0 pr-5 ">
+      {/* Header of the timeline*/}
+      <div className="w-full max-w-2xl py-4 mb-2 flex items-center justify-between">
         <h5 className="text-slate-800 text-4xl font-bold antialiased">Home</h5>
         <button
           onClick={toggleModal}
           className="bg-gradient-to-r from-loomin-yellow to-loomin-orange
-    text-white font-bold py-1 px-4 rounded-2xl hover:scale-105 transform transition-transform
-    "
+          text-white font-bold py-1 px-4 rounded-2xl hover:scale-105 transform transition-transform"
         >
           <i className="bx bx-plus font-sans"></i>
           Post
@@ -60,35 +59,37 @@ const Timeline = () => {
           </div>
         </div>
       )}
-      <InfiniteScroll
-        dataLength={posts.length}
-        next={loadMorePosts}
-        hasMore={hasMore}
-        scrollableTarget="posts-container"
-        loader={
-          <div className="flex justify-center mb-4">
-            <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
-          </div>
-        }
-        endMessage={
-          !posts.length ? (
-            <div className="flex items-center justify-center">
-              <div>
-                <p className="text-center text-gray-500">No posts yet!</p>
-              </div>
-            </div>
-          ) : (
-            <p className="text-center text-gray-500 border-black mb-4">
-              You've seen all posts!
-            </p>
-          )
-        }
-      >
-        {displayPosts()}
-      </InfiniteScroll>
 
+      <div className="w-full max-w-2xl">
+        <InfiniteScroll
+          dataLength={posts.length}
+          next={loadMorePosts}
+          hasMore={hasMore}
+          scrollableTarget="posts-container"
+          loader={
+            <div className="flex justify-center mb-4">
+              <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+            </div>
+          }
+          endMessage={
+            !posts.length ? (
+              <div className="flex items-center justify-center">
+                <div>
+                  <p className="text-center text-gray-500">No posts yet!</p>
+                </div>
+              </div>
+            ) : (
+              <p className="text-center text-gray-500 border-black mb-4">
+                You've seen all posts!
+              </p>
+            )
+          }
+        >
+          {displayPosts()}
+        </InfiniteScroll>
+      </div>
     </div>
   );
 };
 
-      export default Timeline;
+export default Timeline;
